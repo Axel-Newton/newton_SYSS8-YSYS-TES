eb2caffddb0e5b8704b748c49bb71c1adbefd2af
Axel-Newton <axel.larsson@edu.newton.se>
2026-04-08 20:44:20 +0200

    implement integration test using Httpclient and MSTest as well as added postman collection

--- labs/w15/ShoppingCartAppIntegration.Tests/GlobalContext.cs
+++ labs/w15/ShoppingCartAppIntegration.Tests/GlobalContext.cs
@@ -10,7 +10,7 @@ public class GlobalContext
     {
         // Read from .runsettings and store globally once
         appUrl = context.Properties["appUrl"]?.ToString();
-        
+
         if (string.IsNullOrEmpty(appUrl))
         {
             throw new AssertFailedException("appUrl not found in .runsettings file.");
